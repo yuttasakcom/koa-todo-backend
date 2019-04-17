4a9c035067106458816d5bae8287deb375445e70
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-04-17 23:05:50 +0700

    created todos router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+
 import routes from "./routes";
 
 const port = process.env.PORT || "4000";
 const app = new Koa();
 
+app.use(bodyParser());
 app.use(routes.routes());
 app.use(routes.allowedMethods());
 
--- src/routes/api/todos.js
+++ src/routes/api/todos.js
@@ -6,4 +6,20 @@ router.get("/todos", async ctx => {
   ctx.body = "GET todos";
 });
 
+router.get("/todos/:id", async ctx => {
+  ctx.body = `GET todos/${ctx.params.id}`;
+});
+
+router.post("/todos", async ctx => {
+  ctx.body = `todo = ${ctx.request.body.todo}`;
+});
+
+router.put("/todos/:id", async ctx => {
+  ctx.body = `id = ${ctx.params.id} todo = ${ctx.request.body.todo}`;
+});
+
+router.delete("/todos/:id", async ctx => {
+  ctx.body = `id = ${ctx.params.id}`;
+});
+
 export default router.routes();
