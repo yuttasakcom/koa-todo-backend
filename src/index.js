86c766940e1630f1d56a25dacda58b10ec98b60b
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-04-19 04:01:27 +0700

    deploy heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const port = process.env.PORT || "4000";
+const port = process.env.PORT || 3000;
 
 export default app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
